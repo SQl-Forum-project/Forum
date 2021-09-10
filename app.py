59fe1933319c34ef4b4555516716ad76474669d7
YourDad07 <salmanadhikari415@gmail.com>
2021-09-10 22:36:50 +0530

    Fixed Database 2

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.secret_key = 'ye ye'
 class Forumdg(db.Model):
-    __tablename__ = 'forumdg2'
+    __tablename__ = 'forumdg3'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
 
     def __repr__(self) -> str:
         return f"{self.id} - {self.username}"
